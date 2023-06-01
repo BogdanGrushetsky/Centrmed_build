635e8c0a0fd85436df032c95919a8bd4b9ad28fb
Bohdan Hrushetskyi <bogdan1grushetsky@gmail.com>
2023-06-01 12:35:51 +0300

    v "1.0"

---
+++ static/js/510.bb89f92f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[510],{4629:function(n,e,r){r.d(e,{f:function(){return t}});var c=r(184),t=function(n){var e=n.children;return(0,c.jsx)("h1",{className:"TitlePages",children:e})}},4510:function(n,e,r){r.r(e);r(2791);var c=r(4629),t=r(184);e.default=function(){return(0,t.jsx)(t.Fragment,{children:(0,t.jsx)(c.f,{children:"\u0417\u0430\u0431\u0440\u043e\u043d\u044c\u043e\u0432\u0430\u043d\u0438\u0439 \u0447\u0430\u0441"})})}}}]);
+//# sourceMappingURL=510.bb89f92f.chunk.js.map
